bf65eecc102bb32324ec70c8196bdf664d78dd03
kitize00 <kitize00@gmail.com>
2018-04-26 16:48:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,25 +3,31 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
 use \LINE\LINEBot; 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
-$channel_token = '1v2OUa9tuMIiDhEg57ANbsRaBDbBGP9nlCC+Dpvt5HrsQ+LqcrImWPUBkH8re/pwqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLrSkLdcjbFurwXGOyHLt8MdgzLfAe7r0BsQV5cATlUanW3OgJewdB04t89/1O/w1cDnyilFU='; 
-$channel_secret = '9b2c7349ea939ef723a3cb453d774c86'; 
+$channel_token = 'AaC6MGkTB53CyXHhSFQtJ0jB/7N3Us/ZIlUtYpDuaLY59fhT8NRXI13cV3TuBewRIz6To7lN29uWOYAELcimK1ihnHKnN7wZE0F0infoxvns9AAaKMULnrWd9U//hUALyqAIsBTVDt9EVWo0/ly93AdB04t89/1O/w1cDnyilFU='; 
+$channel_secret = '9b9944d9c68676a215b2efa60ae862c9'; 
+
 // Get message from Line API 
 $content = file_get_contents('php://input'); 
 $events = json_decode($content, true); 
 if (!is_null($events['events'])) { 
-// Loop through each event 
+  
+// Loop through each event   
 foreach ($events['events'] as $event) { 
-// Line API send a lot of event type, we interested in message only. 
+  
+// Line API send a lot of event type, we interested in message only.   
 if ($event['type'] == 'message') { 
+  
 // Get replyToken 
 $replyToken = $event['replyToken']; 
 switch($event['message']['type']) { 
 case 'sticker': 
 $messageID = $event['message']['packageId']; 
+    
 // Reply message 
 $respMessage = 'Hello, your Sticker Package ID is '. $messageID; 
 break; 
 default: 
+    
 // Reply message 
 $respMessage = 'Please send Sticker only'; 
 break; 
