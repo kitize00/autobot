5a74fc8a3c4f51cf7d3bd1b917fdb24699965a79
kitize00 <kitize00@gmail.com>
2018-05-12 16:40:10 +0700

    Update index_Video.php

--- index_Video.php
+++ index_Video.php
@@ -8,6 +8,10 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token = 'AaC6MGkTB53CyXHhSFQtJ0jB/7N3Us/ZIlUtYpDuaLY59fhT8NRXI13cV3TuBewRIz6To7lN29uWOYAELcimK1ihnHKnN7wZE0F0infoxvns9AAaKMULnrWd9U//hUALyqAIsBTVDt9EVWo0/ly93AdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '9b9944d9c68676a215b2efa60ae862c9'; 
 
+// LINEBot 
+$httpClient = new CurlHTTPClient($channel_token); 
+$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
+
 // Get message from Line API 
 $content = file_get_contents('php://input'); 
 $events = json_decode($content, true); 
@@ -42,8 +46,7 @@ default:
 $respMessage = 'Please send video only'; 
 break; 
 } 
-$httpClient = new CurlHTTPClient($channel_token); 
-$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
+
 $textMessageBuilder = new TextMessageBuilder($respMessage);
 $response = $bot->replyMessage($replyToken, $textMessageBuilder); 
 } 
