365d0f70f3a7ec6e7c283026814cae855ed3e070
kitize00 <kitize00@gmail.com>
2018-05-12 16:53:00 +0700

    Update and rename index.php to index_Audio.php

--- index_Audio.php
+++ index_Audio.php
@@ -5,6 +5,7 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
 use \LINE\LINEBot; 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
 // Token 
 $channel_token = 'AaC6MGkTB53CyXHhSFQtJ0jB/7N3Us/ZIlUtYpDuaLY59fhT8NRXI13cV3TuBewRIz6To7lN29uWOYAELcimK1ihnHKnN7wZE0F0infoxvns9AAaKMULnrWd9U//hUALyqAIsBTVDt9EVWo0/ly93AdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '9b9944d9c68676a215b2efa60ae862c9'; 
@@ -43,15 +44,15 @@ $respMessage = 'Hello, your audio ID is '. $messageID;
 
 break; 
 default: 
+    
 // Reply message 
 $respMessage = 'Please send audio only';
 break; 
 } 
-$textMessageBuilder = new TextMessageBuilder($respMessage); 
-
+$textMessageBuilder = new TextMessageBuilder($respMessage);
 $response = $bot->replyMessage($replyToken, $textMessageBuilder); 
 
 } 
 } 
 } 
-echo "OK Audio4";
+echo "OK Audio1";
