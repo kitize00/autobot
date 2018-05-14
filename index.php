9526c330292685fa5ab64ae9f8a6630d4180a286
kitize00 <kitize00@gmail.com>
2018-05-14 15:31:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+require_once('./vendor/autoload.php'); 
+
+// Namespace 
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
+// Token 
+$channel_token = '2MCOyCeaBipmw3ZzJG8BrsiO4KzCoaoPddMgbZtEu5HHVeIaWU+PDKcCZRJEY76zqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLrSkLdciLCuKUgV6aFrvAAuuG1mMWe7DCzfEW9FfHQhJR4F/m0AdB04t89/1O/w1cDnyilFU='; 
+$channel_secret = 'd4afd7da941ac195c155fe67dcb5a338'; 
+// Get message from Line API 
+$content = file_get_contents('php://input'); 
+$events = json_decode($content, true); 
+if (!is_null($events['events'])) { 
+// Loop through each event 
+foreach ($events['events'] as $event) { 
+// Line API send a lot of event type, we interested in message only. 
+if ($event['type'] == 'message') { 
+// Get replyToken 
+$replyToken = $event['replyToken']; 
+switch($event['message']['type']) { 
+case 'location': 
+$address = $event['message']['address']; 
+// Reply message 
+$respMessage = 'Hello, your address is '. $address; 
+ 
+break; 
+default: 
+// Reply message 
+$respMessage = 'Please send location only'; 
+break; 
+} 
+$httpClient = new CurlHTTPClient($channel_token); 
+$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
+$textMessageBuilder = new TextMessageBuilder($respMessage); 
+$response = $bot->replyMessage($replyToken, $textMessageBuilder); 
+} 
+}
+} 
+echo "OK";
