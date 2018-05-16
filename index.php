1f7a922d5b8ed311f7cceb0df75ed03edf95bc39
kitize00 <kitize00@gmail.com>
2018-05-16 11:41:49 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+require_once('./vendor/autoload.php'); 
+
+// Namespace 
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
+
+// Token 
+$channel_token = 'AaC6MGkTB53CyXHhSFQtJ0jB/7N3Us/ZIlUtYpDuaLY59fhT8NRXI13cV3TuBewRIz6To7lN29uWOYAELcimK1ihnHKnN7wZE0F0infoxvns9AAaKMULnrWd9U//hUALyqAIsBTVDt9EVWo0/ly93AdB04t89/1O/w1cDnyilFU=';
+$channel_secret = '9b9944d9c68676a215b2efa60ae862c9'; 
+
+// Get message from Line API 
+$content = file_get_contents('php://input'); 
+$events = json_decode($content, true); 
+if (!is_null($events['events'])) { 
+
+// Loop through each event 
+foreach ($events['events'] as $event) { 
+
+// Line API send a lot of event type, we interested in message only. 
+if ($event['type'] == 'message') { 
+
+// Get replyToken 
+$replyToken = $event['replyToken']; 
+switch($event['message']['type']) { 
+case 'file': 
+$messageID = $event['message']['id']; 
+$fileName = $event['message']['fileName']; 
+
+// Reply message 
+$respMessage = 'Hello, your file ID is '. $messageID . ' and file name is '. $fileName; 
+break; 
+default: 
+
+// Reply message 
+$respMessage = 'Please send file only'; 
+break; 
+} 
+$httpClient = new CurlHTTPClient($channel_token); 
+$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
+$textMessageBuilder = new TextMessageBuilder($respMessage); 
+$response = $bot->replyMessage($replyToken, $textMessageBuilder); 
+} 
+}
+} 
+echo "OK File";
