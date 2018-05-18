1340c054a318466f24895f39532930c604635cd0
kitize00 <kitize00@gmail.com>
2018-05-18 16:21:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,12 @@
 <title>Page Title</title>
 </head>
 <body>
-
+<center>
+  echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+  </center>
 <h1>This is a Heading</h1>
 <p>This is a paragraph.</p>
 
