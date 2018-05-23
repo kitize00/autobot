8c001439b56675afcd4951e59b7039ffe6d6a22d
kitize00 <kitize00@gmail.com>
2018-05-23 16:11:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if (!is_null($events['events'])) {
                         $event['source']['userId']);
                     $result = $connection->query($sql);
 
-                    if($result !== false && $result->rowCount() >0) {
+                    if($result == true && $result->rowCount() >0) {
                         // Save database
                         $params = array(
                             'name' => $event['message']['text'],
@@ -77,4 +77,4 @@ if (!is_null($events['events'])) {
 	}
 }
 
-echo "OK Slips1";
+echo "OK Slips2";
