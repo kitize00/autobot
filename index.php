abb313b266d4e80035c908d2a84cc5821543476a
kitize00 <kitize00@gmail.com>
2018-05-28 16:44:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 }
 } 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AaC6MGkTB53CyXHhSFQtJ0jB/7N3Us/ZIlUtYpDuaLY59fhT8NRXI13cV3TuBewRIz6To7lN29uWOYAELcimK1ihnHKnN7wZE0F0infoxvns9AAaKMULnrWd9U//hUALyqAIsBTVDt9EVWo0/ly93AdB04t89/1O/w1cDnyilFU=');
+/*$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('AaC6MGkTB53CyXHhSFQtJ0jB/7N3Us/ZIlUtYpDuaLY59fhT8NRXI13cV3TuBewRIz6To7lN29uWOYAELcimK1ihnHKnN7wZE0F0infoxvns9AAaKMULnrWd9U//hUALyqAIsBTVDt9EVWo0/ly93AdB04t89/1O/w1cDnyilFU=');
 
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '9b9944d9c68676a215b2efa60ae862c9']);
 $response = $bot->getMessageContent('8019724954285');
@@ -54,6 +54,6 @@ if ($response->isSucceeded()) {
     echo fwrite($tempfile, $response->getRawBody());
 } else {
     error_log($response->getHTTPStatus() . ' ' . $response->getRawBody());
-}
+}*/
 echo "OK FileGroup1";
 ?>
