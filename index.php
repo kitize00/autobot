7b88c95e1b2d5c72ced26783018045d2363fd3a8
kitize00 <kitize00@gmail.com>
2018-09-04 21:23:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,55 @@
-<?php 
-require_once('./vendor/autoload.php');
+<?php
+require_once('./vendor/autoload.php'); 
 
- // Namespace 
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
-use \LINE\LINEBot; 
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
+// Namespace 
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+ 
+// Token
+$channel_token = 
+'1v2OUa9tuMIiDhEg57ANbsRaBDbBGP9nlCC+Dpvt5HrsQ+LqcrImWPUBkH8re/pwqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLr
+SkLdcjbFurwXGOyHLt8MdgzLfAe7r0BsQV5cATlUanW3OgJewdB04t89/1O/ w1cDnyilFU='; 
+$channel_secret = '9b2c7349ea939ef723a3cb453d774c86';
+ 
+// Get message from Line API
+$content = file_get_contents('php:// input') ;
+$events = json_decode($content, true); 
 
-$channel_token = 'AaC6MGkTB53CyXHhSFQtJ0jB/7N3Us/ZIlUtYpDuaLY59fhT8NRXI13cV3TuBewRIz6To7lN29uWOYAELcimK1ihnHKnN7wZE0F0infoxvns9AAaKMULnrWd9U//hUALyqAIsBTVDt9EVWo0/ly93AdB04t89/1O/w1cDnyilFU='; 
+if (!is_null( $events['events'])) { 
 
-$channel_secret = '9b9944d9c68676a215b2efa60ae862c9'; 
+    // Loop through each event
+    foreach ($events['events'] as $event) { 
 
-// Get message from Line API 
-$content = file_get_contents('php://input');
- $events = json_decode($content, true); 
+        // Line API send a lot of event type, we interested in message only. 
+        if ($event['type'] == 'message') {
+ 
+            // Get replyToken
+            $replyToken = $event['replyToken']; 
 
-if (!is_null($events['events'])) { 
+            switch($event['message']['type']) { 
+                case 'file': 
+                    $messageID = $event['message'][ 'id'];
+                    $fileName = $event['message']['fileName']; 
 
-// Loop through each event
- foreach ($events['events'] as $event) { 
+                    // Reply message 
+                    $respMessage = 'Hello, your file ID is '. $messageID . ' and file name is '.$fileName;
+                    break; 
+                default: 
+                    // Reply message 
+                    $respMessage = 'Please send file only';
+                    break; 
+            }
+ 
+            $httpClient = new CurlHTTPClient($channel_token);
+            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
 
-// Line API send a lot of event type, we interested in message only. 
-if ($event['type'] == 'message') { 
-switch($event['message']['type']) { 
-case 'text': 
+            $textMessageBuilder = new TextMessageBuilder($respMessage) ; 
+            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+        } 
+    }
+}
 
-// Get replyToken 
-$replyToken = $event['replyToken']; 
-
-// Reply message 
-$respMessage = 'Hello, your message is '. $event['message']['text']; 
-
-$httpClient = new CurlHTTPClient($channel_token); 
-$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
-  
-  $textMessageBuilder = new TextMessageBuilder($respMessage); 
-$response = $bot->replyMessage($replyToken, $textMessageBuilder); 
-break; 
-} 
-} 
- } 
-} 
 echo "OK FileGroup1";
 
 
