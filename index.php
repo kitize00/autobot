ac7a4335acb642a96aa050e4064f800a499a703d
kitize00 <kitize00@gmail.com>
2018-09-04 20:16:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 
 
 <?php 
-require_once('./vendor/autoload.php');
- // Namespace 
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
-use \LINE\LINEBot; 
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
 $channel_token = 'AaC6MGkTB53CyXHhSFQtJ0jB/7N3Us/ZIlUtYpDuaLY59fhT8NRXI13cV3TuBewRIz6To7lN29uWOYAELcimK1ihnHKnN7wZE0F0infoxvns9AAaKMULnrWd9U//hUALyqAIsBTVDt9EVWo0/ly93AdB04t89/1O/w1cDnyilFU='; 
 $channel_secret = '9b9944d9c68676a215b2efa60ae862c9'; 
 // Get message from Line API 
@@ -21,7 +16,7 @@ case 'text':
 // Get replyToken 
 $replyToken = $event['replyToken']; 
 // Reply message 
-$respMessage = 'Hello, your message is ; 
+$respMessage = 'Hello, your message is '. $event['message']['text']; 
 $httpClient = new CurlHTTPClient($channel_token); 
 $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); $textMessageBuilder = new TextMessageBuilder($respMessage); 
 $response = $bot->replyMessage($replyToken, $textMessageBuilder); 
