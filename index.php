5c4e3edd33374ca098f697808c3765337e6804d1
kitize00 <kitize00@gmail.com>
2018-09-04 20:30:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (!is_null($events['events'])) {
 // Line API send a lot of event type, we interested in message only. 
 if ($event['type'] == 'message') { 
 switch($event['message']['type']) { 
-case 'text': 
+case 'file': 
 // Get replyToken 
 $replyToken = $event['replyToken']; 
 // Reply message 
